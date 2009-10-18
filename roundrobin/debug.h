b98be106355ad0fac916c3ed69cb48ecdd11cef8
Compy <jimmy@catster.net>
2009-10-18 17:54:51 -0400

    Repository restructure

---
+++ roundrobin/debug.h
@@ -0,0 +1,30 @@
+#ifndef DEBUG_H
+#define DEBUG_H
+
+/* This header file defines all debugging constants and macros, and declares
+ * some variables. Certain debugging features redefine standard constants
+ * and macros. Therefore, this header file should be included after the
+ * other kernel headers.
+ */
+
+#include <ansi.h>
+#include "config.h"
+
+/* Enable prints such as
+ *  . send/receive failed due to deadlock or dead source or dead destination
+ *  . trap not allowed
+ *  . bogus message pointer
+ *  . kernel call number not allowed by this process
+ *
+ * Of course the call still fails, but nothing is printed if these warnings
+ * are disabled.
+ */
+#define DEBUG_ENABLE_IPC_WARNINGS	1
+#define DEBUG_STACKTRACE		1
+#define DEBUG_TIME_LOCKS		1
+
+/* Runtime sanity checking. */
+#define DEBUG_VMASSERT			1
+#define DEBUG_SCHED_CHECK		0
+
+#endif /* DEBUG_H */
